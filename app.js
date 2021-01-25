0d7ae41d571b622971d2c1a6762bb2d1bfc056c8
Isaiah Anthony <isaiah.anthony99@gmail.com>
2021-01-25 14:16:02 -0800

    building

---
+++ app.js
@@ -0,0 +1,28 @@
+import {
+  BrowserRouter as router,
+  Switch, 
+  Route
+  Link 
+ } from 'react-router-dom'
+import Home from './pages/home'
+import Support from './pages/support'
+
+const app = () => {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/support">Support</Link>
+        </nav>
+        <Switch>
+          <Route exact path="/">
+          </Route>
+          <Route path="/support">
+          </Route>
+          </Switch>
+        </Switch>
+      </div>
+    </Router>
+  )
+}
---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const { join } = require('path')
+
+const app = express()
+
+app.use(express.static(join(__dirname, 'public')))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.listen(process.env.PORT || 3000)
